b334514437d339facf7658695228a1e4be39ff51
ILLUSE <codns0621@gmail.com>
2024-07-09 01:39:38 +0900

    add linked list(unfinished) -Request:delete function

--- Review_Project/Alghorithm_linked_list.cpp
+++ Review_Project/Alghorithm_linked_list.cpp
@@ -14,10 +14,11 @@ void displayList(node* head) { //헤드를 입력받고 헤드에서부터 출력
 		printf("\t%d->", p->data); //\t : tab  //현재 위치한 노드의 데이터 출력
 		p = p -> next; //다음 노드로 이동
 	}
+
+	printf("\n\n");
 }
 
-
-node* createLinkedList(int n) {
+node* createLinkedList(int n) { //초기 리스트 생성
 
 	int i = 0;
 	node* head = NULL; //리스트 생성을 위한 구조체들 초기화
@@ -46,18 +47,97 @@ node* createLinkedList(int n) {
 	return head;
 }
 
+node* insertNode(node* head, int value) {
+	int index;
+
+	printf("\nWhere do you want to insert? (0: HEAD , -1: TAIL) : ");
+	scanf_s("%d", &index);
+
+	node* temp = (node*)malloc(sizeof(node)); // 새 노드 동적 할당
+	temp->data = value;
+	temp->next = NULL;
+
+	if (index == 0) { //맨 앞 삽입
+		temp -> next = head;
+		head = temp;
+		printf("\nYou insert Node at HEAD / value : %d\n\n", value);
+	}
+	else if (index == -1) { //맨 뒤 삽입
+		
+		node* p = head;
+		while (p->next != NULL) //p를 리스트의 맨 끝까지 이동시킴
+			p = p->next;
+
+		p->next = temp;
+
+		printf("\nYou insert Node at last / value : %d\n\n", value);
+	}
+	else { //특정 인덱스에 삽입
+		
+		if (index < 0) {
+			printf("Invalid index\n");
+			return head;
+		}
+
+		int count = 1;
+		node* p = head;
+		while (p != NULL && count < index) {
+		
+			p = p->next;
+			count++;
+		
+		}
+
+		if (p == NULL) {
+			printf("Index out of bounds\n");
+		}
+		else {
+			temp->next = p->next; // 새 노드의 next를 p의 다음 노드로 설정
+			p->next = temp; // p의 next를 새 노드로 설정
+		}
+
+		printf("\nYou insert Node at %d / value : %d\n\n",index, value);
+	}
+
+	return head;
+}
+
+void freeLinkedList(node* head) { 
+
+	node* p = NULL; //임시 포인터 선언
+	
+	
+	while (head != NULL) { //p를 리스트의 맨 끝까지 이동시킴
+		
+		p = head; //임시 포인터에 현재 주소 저장
+		head = head->next; //헤드 포인터를 다음 주소로 이동
+		free(p); //현재 위치한 노드 free
+		
+	}
+}
+
 
 int main() {
 
 	int n = 0;
 	node* HEAD = NULL;
+
 	printf("How many node do you want to make? : ");
 	scanf_s("%d", &n);
 	HEAD = createLinkedList(n);
 	printf("\n");
+	//노드 삽입 전
+	displayList(HEAD);
+	
+	HEAD = insertNode(HEAD, 6);
+	HEAD = insertNode(HEAD, 7);
+	HEAD = insertNode(HEAD, 8);
+
+
+	//노드 삽입 후
 	displayList(HEAD);
 
-
+	freeLinkedList(HEAD);
 	return 0;
 
 }
